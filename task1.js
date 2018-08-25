d2f2c3d778eadc77090024314e2cfd83d12df254
小石取り <b1006039@gmail.com>
2018-08-26 06:14:24 +0900

    Javascript Task ver2
    1st : 8400511, len : 686
    2nd : 6649279, len : 665
    Answer: 6649279

--- task1.js
+++ task1.js
@@ -1,46 +1,50 @@
-# For any positive integer n we define two rules:
-# if even: divide by two
-# if odd: multiply by three, then add one, and repeat until the result is the number 1
+// For any positive integer n we define two rules:
+// if even: divide by two
+// if odd: multiply by three, then add one, and repeat until the result is the number 1
 class model1{
-  init(){
-    this.array = new Array();
-  }
-
-# if even: divide by two
-# if odd: multiply by three, then add one, and repeat until the result is the number 1
-  func1(input){
-    if( input % 2 == 0 )
-      return input / 2;
-    else
-      return input * 3 + 1;
-  }
-
-# Create nums array that be created from the above rules.
-  set get_array( in_num ){
-    var output = in_num;
-    this.array.push(output);
-    while( output != 1  ){
-      output = this.func1( output );
-      this.array.push(output);
+    init(){
+        this.ans_array = new Array();
+        this.ans_array[1] = 1;
     }
-  }
+
+// if even: divide by two
+// if odd: multiply by three, then add one, and repeat until the result is the number 1
+    func1(input){
+        if( input % 2 == 0 )
+            return input / 2;
+        else
+            return input * 3 + 1;
+    }
+
+// Create nums array that be created from the above rules.
+    get_array_length( input ){
+        var output = input;
+        var temp_array = new Array();
+        while( output != 1 && ! this.ans_array[ output ] ){
+            temp_array.push( output );
+            output = this.func1( output );
+        }
+        (this.ans_array)[ input ] = temp_array.length + (this.ans_array)[output];
+    }
 }
 
 var obj=new model1;
-var answer = 0;
+obj.init();
+var max_1st_num = 0;
+var max_1st_len = 0;
+var max_2nd_num = 0;
+var max_2nd_len = 0;
+
 var output_length = 0;
-for ( var i = 2; i<100000; i++ ){
-  obj.init();
-  obj.get_array = i;
-  if( output_length < obj.array.length ){
-    output_length = obj.array.length;
-    answer = i;
-  }
+for ( var i = 2; i <= 10000000; i++ ){
+    obj.get_array_length(i);
+    if( max_1st_len < obj.ans_array[ i ] ){
+      max_2nd_num = max_1st_num;
+      max_2nd_len = max_1st_len;
+      max_1st_num = i;
+      max_1st_len = obj.ans_array[ i ];
+    }
 }
 
-obj.init();
-obj.get_array = answer;
-console.log( "num : " + answer );
-console.log( "length : " + output_length );
-console.log( obj.array );
-
+console.log( "1st : " + max_1st_num + ", len : " + max_1st_len );
+console.log( "2nd : " + max_2nd_num + ", len : " + max_2nd_len );
